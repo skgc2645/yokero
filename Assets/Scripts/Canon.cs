a2810d6c4c624f684758372f2de3ffa81d7497d1
skgc2645 <skgcwork2645@gmail.com>
2024-06-08 19:53:00 +0900

    add Canonball Prefab and scripts

---
+++ Assets/Scripts/Canon.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void MoveRight(float speed)
+    {
+
+    }
+
+
+    public void MoveLeft(float speed)
+    {
+
+    }
+
+
+    public void MoveUp(float speed)
+    {
+
+    }
+
+
+    public void MoveDown(float speed)
+    {
+
+    }
+}
