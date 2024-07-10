ac17b4ca3b1d94517000f577a8d94ecc5c33da0d
skgc2645 <skgcwork2645@gmail.com>
2024-07-11 01:42:21 +0900

    fix flag process

--- Assets/Scripts/PlayerCon.cs
+++ Assets/Scripts/PlayerCon.cs
@@ -21,55 +21,77 @@ public enum PlayerEmote
 }
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerCon : MonoBehaviour
 {
+    //property
+    public IReadOnlyReactiveProperty<int> HitPoint { get { return _hitPoint; } }
+    public int EmoteCount {get{return _emoteCount; }}
     //member
     Animator _animator;
-
     int _curEmoteIdx;
-
     bool _isMoving = false;
-
-
+    int _emoteCount = 0;
+    readonly ReactiveProperty<int> _hitPoint = new ReactiveProperty<int>(PLAYER_LIFE_MAX);
+    //íËêî
+    const float   OFFSET      = 3f;
+    const float LIMIT_WIDTH  = 4f;
+    const float   LIMIT_HIGHT = 4f;
+    int PLAYER_EMOTE_NUM = Enum.GetValues(typeof(PlayerEmote)).Length;
+    static int PLAYER_LIFE_MAX = 5;
     Vector3 INITIAL_POS = new Vector3(0.5f, 0.65f, -0.2f);
-    Vector3 SCALE_LEFT  = new Vector3(0.23f, 0.23f, 0.23f);
+    Vector3 SCALE_LEFT = new Vector3(0.23f, 0.23f, 0.23f);
     Vector3 SCALE_RIGHT = new Vector3(-0.23f, 0.23f, 0.23f);
 
-    float   OFFSET      = 3f;
-    float   LIMIT_WIDTH  = 4f;
-    float   LIMIT_HIGHT = 4f;
 
-    int PLAYER_EMOTE_NUM = Enum.GetValues(typeof(PlayerEmote)).Length;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        _animator = GetComponent<Animator>();
-        MoveBtnSettings();
-        Initialize();
-
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
 
-    void Initialize()
+    public void Initialize()
     {
+        _animator = GetComponent<Animator>();
+        MoveBtnSettings();
         transform.position = INITIAL_POS;
+        _emoteCount = 0;
+        _isMoving = false;
     }
 
     void MoveBtnSettings()
     {
-        InputHundler.instance.BtnLeft  .Where(x => x).Subscribe(x => { MoveLeft(); }).AddTo(this);
-        InputHundler.instance.BtnRight .Where(x => x).Subscribe(x => { MoveRight(); }).AddTo(this);
-        InputHundler.instance.BtnUp    .Where(x => x).Subscribe(x => { MoveUp(); }).AddTo(this);
-        InputHundler.instance.BtnDown  .Where(x => x).Subscribe(x => { MoveDown(); }).AddTo(this);
-        InputHundler.instance.BtnEmote .Where(x => x).Subscribe(x => { EmoteAnimation(); }).AddTo(this);
+        InputHundler.instance.BtnLeft  .Where(x => x).Subscribe(x => { Move(MoveDirection.Left); }).AddTo(this);
+        InputHundler.instance.BtnRight .Where(x => x).Subscribe(x => { Move(MoveDirection.Right); }).AddTo(this);
+        InputHundler.instance.BtnUp    .Where(x => x).Subscribe(x => { Move(MoveDirection.Up); }).AddTo(this);
+        InputHundler.instance.BtnDown  .Where(x => x).Subscribe(x => { Move(MoveDirection.Down); }).AddTo(this);
+        InputHundler.instance.BtnEmote .Where(x => x).Subscribe(x => { EmoteAnimation(); _emoteCount++; }).AddTo(this);
+    }
+
+    void Move(MoveDirection dir)
+    {
+        if (!GameFlow.instance.IsGame) return;
+        switch (dir) 
+        { 
+            case MoveDirection.Left:
+                MoveLeft(); break;
+            case MoveDirection.Right:
+                MoveRight(); break;
+            case MoveDirection.Up:
+                MoveUp(); break;
+            case MoveDirection.Down:
+                MoveDown(); break;
+            default:
+                Debug.LogError("input direction error");
+                break;
+        }
     }
 
 
@@ -201,7 +223,10 @@ public class NewBehaviourScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Canon")
-            Debug.Log("hit");
+        if (collision.gameObject.tag == "Canon" && GameFlow.instance.IsGame)
+        {
+            _hitPoint.Value--;
+            if(_hitPoint.Value == 0) GameFlow.instance.IsGame = false;
+        }
     }
 }
